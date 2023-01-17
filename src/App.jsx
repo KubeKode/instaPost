eb580e831873d70075d83938b25fcc40488f98ac
tush-tr <tusharrajput604@gmail.com>
2023-01-17 16:06:45 +0530

    rm vite

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-import HomePage from "./Pages/Home";
-function App() {
-  return <HomePage />;
-}
-
-export default App;
--- src/Components/Header/Index.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Header = () => {
-  return <Wrapper>InstaPost.ai</Wrapper>;
-};
-const Wrapper = styled.div`
-  font-weigth: bolder;
-`;
-export default Header;
